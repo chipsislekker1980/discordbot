9a017100834ac3b7a53cf5763ecad37b37b06539
chipsislekker1980 <65949730+chipsislekker1980@users.noreply.github.com>
2020-06-17 12:11:06 +0200

    Updat index.js
    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ bot.commands = new discord.Collection();
 
 const client = new discord.Client();
 
-bot.login(process.env.token);
 
 fs.readdir("./commands/" , (err, files) => {
+
+    client.login(process.env.token);
 
     if(err) console.log(err);
 
