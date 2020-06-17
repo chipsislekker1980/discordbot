1879153fbd143af8f7ac3f711449f74859519c18
chipsislekker1980 <65949730+chipsislekker1980@users.noreply.github.com>
2020-06-17 18:00:13 +0200

    Update invite.js
    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ module.exports.run = async(bot, message, args) => {
 
 try{
 
-    var text = "Hey, leuk dat je Clearance wilt inviten in je server! Hier is de link: https://discord.com/oauth2/authorize?client_id=720882384941350983&scope=bot \n \n **Support server: https://discord.gg/5zycST8**";
+    var text = "Hey, leuk dat je Clearance wilt inviten in je server! Hier is de link: https://discord.com/oauth2/authorize?client_id=722842476318031884&scope=bot \n \n **Support server: https://discord.gg/5zycST8**";
 
     message.author.send(text);
 
