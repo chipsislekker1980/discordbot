9ee7b17fd150780023425e50e23bb9ac705d142c
chipsislekker1980 <65949730+chipsislekker1980@users.noreply.github.com>
2020-06-18 07:44:12 +0200

    Update ticket.js
    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -35,27 +35,23 @@ module.exports.run = async (bot, message, args) => {
        (createdChannel) => {
            createdChannel.setParent(categoryID).then(
                (settedParent) => {
-                   
-                    settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === '@everyone'),{
-                        SEND_MASSAGES: false,
-                        VIEW_CHANNEL: false
-                    });
+               
+                settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === '@everyone'), {
+                    SEND_MESSAGES: false,
+                    VIEW_CHANNEL: false
+                });
 
-                    settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === 'Support'),{
-                        SEND_MASSAGES: true,
-                        VIEW_CHANNEL: true
-                    });
+                settedParent.updateOverwrite(message.author, {
+                    CREATE_INSTANT_INVITE: false,
+                    VIEW_CHANNEL: true,
+                    READ_MESSAGES: true,
+                    SEND_MESSAGES: true,
+                    ATTACH_FILES: true,
+                    CONNECT: true,
+                    ADD_REACTIONS: true,
+                    READ_MESSAGES_HISTORY: true
 
-                    settedParent.updateOverwrite(message.author.id,{
-                        CREATE_INSTANT_INVITE: false,
-                        READ_MESSAGES: true,
-                        SEND_MASSAGES: true,
-                        ATTACH_FILES: true,
-                        CONNECT: true,
-                        ADD_REACTIONS: true,
-                        READ_MESSAGES_HISTORY: true
-                        
-                    });
+                });
 
                     var embedParent = new discord.MessageEmbed()
                     .setTitle(`Hallo ${message.author.username}`)
