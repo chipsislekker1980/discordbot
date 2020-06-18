06cf725f9848e9fc26067d94b0fd2f1dc35fe455
chipsislekker1980 <65949730+chipsislekker1980@users.noreply.github.com>
2020-06-18 16:39:29 +0200

    Summary (required)

---
+++ commands/idee.js
@@ -0,0 +1,44 @@
+const discord = require("discord.js")
+const botConfig = require("../botConfig.json")
+const c = client.channels.cache.get("720642251738316860");
+
+module.exports.run = async(bot, message, args) => {
+
+    var prefix = botConfig.prefix;
+
+    var item = message.content.slice(6);
+
+
+        if (!item) {
+
+            var noSuggestEmbed = new discord.MessageEmbed()
+                .setTitle("❌ Geef een idee mee!")
+                .setDescription("Zonder idee kan ik niets sturen!")
+                .setColor("#0000ff")
+            return message.channel.send(noSuggestEmbed);
+        }
+    
+        /////////////////////////
+    
+        var suggestEmbed = new discord.MessageEmbed()
+            .setAuthor(message.member.nickname ? message.member.nickname : message.author.tag, message.author.displayAvatarURL())
+            .setColor("#ffd700")
+            .setTitle("Nieuwe idee!")
+            .setDescription(item)
+            .setThumbnail(bot.user.displayAvatarURL())
+            .setTimestamp()
+            .setFooter(" | Clearance idee systeem |", bot.user.displayAvatarURL())
+        c.send(suggestEmbed).then((message) => {
+    
+            const send = message;
+            sent.react(`👍`).then(() => {
+                sent.react(`👎`)
+            })
+    
+        })
+    }
+
+
+module.exports.help = {
+    name: "idee"
+}
